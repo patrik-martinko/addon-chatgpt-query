77d26da7ceafcdb95d7ab79eb23823b744e3e915
Leonard Z <37145130+2482103133@users.noreply.github.com>
2023-02-28 22:46:15 +0800

    Fix issue
    When the query is too long, it will be cut off

--- extension/script.js
+++ extension/script.js
@@ -1,4 +1,12 @@
-let query = (new URLSearchParams(location.search)).get('q');
+// Get the current URL
+const currentUrl = window.location.href;
+
+// Parse the URL and get the search params
+const urlSearchParams = new URLSearchParams(currentUrl.split('?')[1]);
+
+// Get the value of the "q" parameter
+const query = urlSearchParams.get('q');
+
 if (query) {
 	query = decodeURIComponent(query);
 	const interval = setInterval(() => {
