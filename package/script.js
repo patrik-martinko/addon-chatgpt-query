81f206d134f63db1cb869bb8b2c91b239fb8a18d
Blaise Kal <blaisekal@gmail.com>
2023-10-29 11:57:40 +0100

    Update dom query selectors
    The query selectors seem to be outdated.

--- package/script.js
+++ package/script.js
@@ -2,14 +2,14 @@ let query = (new URLSearchParams(location.search)).get('q');
 if (query) {
 	query = decodeURIComponent(query);
 	const interval = setInterval(() => {
-		const input = document.querySelector('[data-id="root"]');
+		const input = document.querySelector('#prompt-textarea');
 		if (input) {
 			if (input.value === query) {
 				input.dispatchEvent(new Event('input', { bubbles: true }));
-				input.nextSibling.click();
+				document.querySelector('[data-testid="send-button"]').click();
 				clearInterval(interval);
 			}
 			input.value = query;
 		}
-	}, 500);
+	}, 200);
 }
